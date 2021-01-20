36d3ed87fd060c4b0c5fac1522d900ce92271e99
441122229944 <davidtherion412294@gmail.com>
2021-01-20 02:06:28 -0300

    new updates

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+    Route::get('/login', function () {
+        return 'login';
+    })->name('login');
 
+    Route::get('/', function () {
+        return view('welcome');
+        //return view('app');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/clientes', function () {
+        return view('clientes');
+        //return view('app');
+    });
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/produtos', function () {
+            return view('produtos');
+        });
+    });
